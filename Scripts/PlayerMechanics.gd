0da0baea5c63088ad34bd08fce86e1bd13dbf59a
neoharry Mihailidis <neoharry10@outlook.com.gr>
2017-08-06 01:55:43 +0300

    Fixed Shooting mechs,cleaned files

--- Scripts/PlayerMechanics.gd
+++ Scripts/PlayerMechanics.gd
@@ -2,37 +2,46 @@ extends KinematicBody2D
 
 export var MOTION_SPEED = 140
 export var SPRINT_SPEED = 60
+
 const MAX_SPEED = 240
 
 var offset 
 var relative_mouse_pos 
 var RayNode 
+var ShotR
+var motion
 
-#var canShoot = true
 var hasShot = false
-#var alive = true
 
 
-
+#executes on active scene
 func _ready():
 	set_fixed_process(true)
 	
 #onready variables 
 func _fixed_process(delta):
-	var motion = Vector2()
+	motion = Vector2()
 	offset = Vector2()
 	relative_mouse_pos = Vector2()
 	RayNode = get_node("RayCast2D")
 		
-	#max speed
 		
+	#crosshair 
+	offset = -get_viewport().get_canvas_transform().o * get_node("Camera2D").get_zoom() 
+	relative_mouse_pos = get_viewport().get_mouse_pos() * get_node("Camera2D").get_zoom() + offset 
+	get_node("Gun").look_at(relative_mouse_pos)
+	get_node("/root/World/Crosshair").set_pos(relative_mouse_pos)
+		
+	#Gun rotation in radians
+	ShotR = get_node("Gun").get_rot()
+		
+		
+	#max speed,for stability 
 	if (MOTION_SPEED > MAX_SPEED):
 		MOTION_SPEED = MAX_SPEED
 		
 		
-		
-	#movement here
-		
+	#movement and rotation 
 	if Input.is_action_pressed("ui_up"):
 		motion += Vector2(0, -1)
 		RayNode.set_rotd(180)
@@ -47,56 +56,23 @@ func _fixed_process(delta):
 		RayNode.set_rotd(90)
 	var firing = Input.is_action_pressed("fire")
 		
-	#sprint mech
+	#sprint 
 	if Input.is_action_pressed("sprint"):
 	    motion = motion.normalized()*(MOTION_SPEED + SPRINT_SPEED)*delta
 	else:
 		motion = motion.normalized()*MOTION_SPEED*delta
 		
-		
 	move(motion)
 		
 		
 	#Shooting here
-		
-		
 	if (firing and hasShot == false):
 		var shot = preload("res://Scenes/Bullet.tscn").instance()
 		shot.set_pos(get_node("Gun/GunPos").get_global_pos())
+		#apply the gun rotation to the bullet
+		shot.rotate(ShotR)
 		get_node("/root/World/BulletPar").add_child(shot)
-		var shot = preload("res://Scenes/Bullet.tscn").instance()
-		shot.set_pos(get_node("Gun/GunPos").get_global_pos())
-		get_node("/root/World/BulletPar").add_child(shot)
-		
-		
 		
 	#shot delay
-		
+	#DO NOT CHANGE!!
 	hasShot = firing
-		
-		
-	#croshair on mouse
-		
-	offset = -get_viewport().get_canvas_transform().o * get_node("Camera2D").get_zoom() # Get the offset
-	relative_mouse_pos = get_viewport().get_mouse_pos() * get_node("Camera2D").get_zoom() + offset # And add it to the mouse position
-		
-		
-	get_node("Gun").look_at(relative_mouse_pos)
-	get_node("/root/World/Crosshair").set_pos(relative_mouse_pos)
-		
-		
-		
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-
--- Scripts/bulletR.gd
+++ Scripts/bulletR.gd
@@ -1,18 +1,15 @@
 extends Area2D
 
-var B_SPEED = 300
+var B_SPEED = 410
 
 var hit = false
 
 
 func _process(delta):
-	translate(Vector2(delta*B_SPEED, 0))
-
-
+	translate(Vector2(0, delta*B_SPEED))
+	
 func _ready():
 	set_process(true)
-
-
 	
 func _on_visibility_exit_screen():
 	queue_free()
